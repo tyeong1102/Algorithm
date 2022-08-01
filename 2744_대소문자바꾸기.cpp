e0824159b663ed033b34f98182d544072ca9c1fb
Taeyeong <tyeong1102@gmail.com>
2022-08-01 15:50:46 +0900

    Create 2744_대소문자바꾸기.cpp

---
+++ 2744_대소문자바꾸기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    for(int i=0; i<s.size(); i++){
+        if(s[i] > 96){
+            s[i] -= 32;
+        } else{
+            s[i] += 32;
+        }
+    }
+    
+    cout << s;
+}
