bb7b3fd305dd6361ba2423a37d0d9c214c490429
Taeyeong <tyeong1102@gmail.com>
2022-08-11 19:24:29 +0900

    Update 11052_카드구매하기.cpp

--- 11052_카드구매하기.cpp
+++ 11052_카드구매하기.cpp
@@ -15,6 +15,7 @@ int main(){
     for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++){
             d[i] = max(d[i], d[i-j]+p[j]);
+            // i-j만큼의 카드를 최대값으로 구매하고 마지막 j개를 구매 하였을 때
         }
     }
     
