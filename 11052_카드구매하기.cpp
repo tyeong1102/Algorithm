995e8f77be44e10ccd50fed74fe768f5b43f9ee5
Taeyeong <tyeong1102@gmail.com>
2022-08-11 19:17:39 +0900

    Create 11052_카드구매하기.cpp

---
+++ 11052_카드구매하기.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> p(n+1);
+    vector<int> d(n+1);
+
+    for(int i=1; i<=n; i++){
+        cin >> p[i];
+    }
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            d[i] = max(d[i], d[i-j]+p[j]);
+        }
+    }
+    
+    cout << d[n] << '\n';
+}
