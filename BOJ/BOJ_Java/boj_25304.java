93f623926627669667a975aa75253e9c4dd962f7
tyeong1102 <tyeong1102@gmail.com>
2022-12-20 18:32:33 +0900

    Delete boj_25304.java

--- BOJ/BOJ_Java/boj_25304.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class boj_25304 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a;
-        ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹㅁ;
-    }
-}
