f60e5fea4758c8ab4cac837e6887b5f62bbb4633
Kim Taeyeong <tyeong1102@gmail.com>
2023-06-08 16:55:45 +0900

    최댓값과 최솟값

---
+++ Programmers/Lv2/Lv2_최댓값과 최솟값.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+
+        String[] arr = s.split(" ");
+        int[] nums = new int[arr.length];
+
+        for(int i = 0; i < arr.length; i++) {
+            nums[i] = Integer.parseInt(arr[i]);
+        }
+
+        Arrays.sort(nums);
+
+        answer = nums[0] + " " + nums[nums.length - 1];
+
+        return answer;
+    }
+}
