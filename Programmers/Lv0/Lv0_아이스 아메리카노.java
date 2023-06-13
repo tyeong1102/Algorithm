2b035bbe74933ea747e30548f50d127365d1abdc
Kim Taeyeong <tyeong1102@gmail.com>
2023-06-13 22:46:19 +0900

    아이스 아메리카노

--- Programmers/Lv0/Lv0_아이스 아메리카노.java
+++ Programmers/Lv0/Lv0_아이스 아메리카노.java
@@ -1,17 +1,9 @@
 class Solution {
-    public int solution(int angle) {
-        int answer = 0;
+    public int[] solution(int money) {
+        int coffee = money / 5500;
+        int change = money % 5500;
 
-        if (angle > 0 && angle < 90) {
-            answer = 1;
-        } else if (angle == 90) {
-            answer = 2;
-        } else if (angle > 90 && angle < 180) {
-            answer = 3;
-        } else if (angle == 180) {
-            answer = 4;
-        }
-
+        int[] answer = {coffee, change};
         return answer;
     }
 }
