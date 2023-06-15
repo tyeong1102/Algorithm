05b80657790f81e90b4160253230c0e601e825f9
Kim Taeyeong <tyeong1102@gmail.com>
2023-06-15 21:18:20 +0900

    스택

--- BOJ/Sliver/Silver4/BOJ_10828.java
+++ BOJ/Sliver/Silver4/BOJ_10828.java
@@ -1,11 +1,12 @@
 import java.io.*;
 import java.util.Stack;
 
-public class boj_10828 {
+public class BOJ_10828 {
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         Stack<Integer> s = new Stack<>();
 
@@ -20,31 +21,31 @@ public class boj_10828 {
                     break;
                 case "pop":
                     if (s.empty()) {
-                        bw.write("-1" + "\n");
+                        sb.append("-1" + "\n");
                     } else {
-                        bw.write(s.pop() + "\n");
+                        sb.append(s.pop() + "\n");
                     }
                     break;
                 case "size":
-                    bw.write(s.size() + "\n");
+                    sb.append(s.size() + "\n");
                     break;
                 case "empty":
                     if (s.empty()) {
-                        bw.write("1" + "\n");
+                        sb.append("1" + "\n");
                     } else {
-                        bw.write("0" + "\n");
+                        sb.append("0" + "\n");
                     }
                     break;
                 case "top":
                     if (s.empty()) {
-                        bw.write("-1" + "\n");
+                        sb.append("-1" + "\n");
                     } else {
-                        bw.write(s.peek() + "\n");
+                        sb.append(s.peek() + "\n");
                     }
                     break;
             }
         }
-        bw.flush();
-        bw.close();
+
+        System.out.println(sb);
     }
 }
