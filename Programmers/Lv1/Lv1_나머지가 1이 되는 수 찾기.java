98c627ff2f0f63eb6ae183213cf147f9be0b706d
Kim Taeyeong <tyeong1102@gmail.com>
2023-06-17 23:20:38 +0900

    나머지가 1이 되는 수 찾기

---
+++ Programmers/Lv1/Lv1_나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        answer = (double) sum / arr.length;
+
+        return answer;
+    }
+}
