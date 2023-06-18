b1b92ac13da2fa188b83e38c16f54ead359908a9
Kim Taeyeong <tyeong1102@gmail.com>
2023-06-18 23:12:51 +0900

    JadenCase 문자열 만들기

--- Programmers/Lv2/Lv2_JadenCase 문자열 만들기.java
+++ Programmers/Lv2/Lv2_JadenCase 문자열 만들기.java
@@ -1,20 +1,27 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s) {
-        String answer = "";
 
-        String[] arr = s.split(" ");
-        int[] nums = new int[arr.length];
+        StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < arr.length; i++) {
-            nums[i] = Integer.parseInt(arr[i]);
+        String[] words = s.split(" "); // 공백을 기준으로 split
+
+        for(String word : words) {
+
+            if (word.length() == 0) { // 길이가 0이라는 것은 공백이기 때문에 추가만 한다.
+                sb.append(" ");
+                continue;
+            }
+
+            String head = word.substring(0, 1).toUpperCase(); // 첫 글자 대문자로
+            String tail = word.substring(1, word.length()).toLowerCase(); // 나머지 소문자로
+
+            sb.append(head).append(tail).append(" "); // 띄어쓰기 포함해서 문장으로
         }
 
-        Arrays.sort(nums);
+        if (!s.substring(s.length() -1, s.length()).equals(" ")) { // 원래 있던 공백이 아닐 경우 삭제
+            sb.deleteCharAt(sb.lastIndexOf(" "));
+        }
 
-        answer = nums[0] + " " + nums[nums.length - 1];
-
-        return answer;
+        return sb.toString();
     }
 }
