fa7375e3e926306cd22a3795e34f1b3c86e8e9df
Kim Taeyeong <tyeong1102@gmail.com>
2023-06-30 23:56:14 +0900

    정수 N개의 합

---
+++ BOJ/Bronze/B2_정수 N개의 합.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+long long sum(std::vector<int> &a) {
+    long long ans = 0;
+    int n = a.size();
+    for(int i = 0; i < n; i++){
+        ans += a[i];
+    }
+    return ans;
+}
