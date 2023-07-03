6ce711088188d3a979d4556d535df6d2371bed70
Kim Taeyeong <tyeong1102@gmail.com>
2023-07-03 16:35:48 +0900

    안전 영역

--- BOJ/Sliver/Silver1/S1_안전 영역.cpp
+++ BOJ/Sliver/Silver1/S1_안전 영역.cpp
@@ -1,29 +1,57 @@
 /*
-	변수 하나하나가 20억이 넘는 큰 수이기 때문에 그냥 20억씩 곱하면 long long의 범위를 벗어나는 큰 수가 된다.
-	따라서 2^8 = 2^4 * 2^4이듯 b를 반으로 나눠서 소분해서 계산하고 한번 더 곱해준 다음 %c를 해준다.
-	그런데 홀수일 경우에는 누락되는 수가 하나 생기기 때문에 a를 한번 더 곱해주고 %c를 해주는 것으로 한다.
+	처음에는 단순히 dfs를 사용하여 N * N 크기의 2차원 배열에서 N보다 큰 부분을 영역으로 하여 영역의 개수를 구하면 되는 줄 알았다.
+	예제도 통과하여 문제가 없을 것 같았는데, 높이가 될 수 있는 경우를 모두 검토 해줬어야 했다. 
+	그래서 초기 코드에는 없던 변수 d가 추가되었다.
 */
-
 #include <bits/stdc++.h>
 using namespace std;
 
-long long a, b, c;
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
 
-long long go(long long a, long long b){
-	if(b == 1) return a % c; // 재귀함수는 기저사례
-	long long ret = go(a, b / 2); // 반만 곱함
-	ret = (ret * ret) % c; // 한번 더 곱해줌
-	if(b % 2 == 1) ret = (ret * a) % c; // 홀수일 경우 한번 더 곱함
-	return ret;
-}
+int n, ans = 1;
+int a[101][101], visited[101][101];
 
-int main(){
-    ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+void dfs(int y, int x, int d){
+    visited[y][x] = 1; 
+
+    for(int i = 0; i < 4; i++){
+        int ny = y + dy[i]; 
+        int nx = x + dx[i]; 
+        if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue; 
+        if(!visited[ny][nx] && a[ny][nx] > d) dfs(ny, nx, d);
+    }
+    return;
+} 
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     cout.tie(NULL);
 
-	cin >> a >> b >> c;
+    cin >> n; 
 
-	cout <<  go(a, b) << '\n';
-    return 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    for(int d = 1; d < 101; d++){
+        fill(&visited[0][0], &visited[0][0] + 101 * 101, 0); // 케이스 마다 0으로 초기화
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(a[i][j] > d && !visited[i][j]) {
+                    dfs(i, j, d);
+                    cnt++;
+                }
+            }
+        }
+        ans= max(ans, cnt);
+    }
+
+    cout << ans << '\n';
+    return 0; 
 }
