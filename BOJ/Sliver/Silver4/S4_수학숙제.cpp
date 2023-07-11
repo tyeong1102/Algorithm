82ea3faf3080349c4538420de1f02a8cd8277b33
Kim Taeyeong <tyeong1102@gmail.com>
2023-07-11 17:23:29 +0900

    수학숙

--- BOJ/Sliver/Silver4/S4_수학숙제.cpp
+++ BOJ/Sliver/Silver4/S4_수학숙제.cpp
@@ -1,28 +1,49 @@
+/*
+    각 줄당 100글자라는 것을 보고 string을 떠올려야 한다. int형은 10^9라 10글자까지만 커버 가능하고, long long을 써도 10^18이라 19글자만 커버가능하기 때문.
+    A = 65, a = 97 아스키 코드는 외우자.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, cnt;
-string s;
+int n;
+string s, ans;
+vector<string> v;
 
-int main(){
-    ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-    cout.tie(NULL);
+void go() {
+    while(true) {
+        // 아무것도 없을 경우 참조 에러가 나기 때문에, 사이즈가 있는지 확인하고 앞부분이 0이라면 지워야 한다.
+        if(ans.size() && ans.front() == '0') ans.erase(ans.begin());
+        else break;
+    }
 
-	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> s;
-		stack<char> st;
-		
-		for(char c : s){
-			if(!st.empty() && st.top() == c) st.pop();
-			else st.push(c);
-		}
+    if(ans.size() == 0) ans = "0"; // 0000일 경우 다 사라지기 때문에 0을 할당.
+    
+    v.push_back(ans);
+    ans = "";
+}
 
-		if(st.empty()) cnt++;
-	}
+bool cmp(string a, string b) {
+    if(a.size() == b.size()) return a < b;
+    return a.size() < b.size();
+}
 
-	cout << cnt << '\n';
+int main() {
+    cin >> n;
 
+    for(int i = 0; i < n; i++) {
+        cin >> s;
+        ans = "";
+
+        for(int j = 0; j < s.size(); j++) {
+            if(s[j] < 65) ans += s[j];
+            else if(ans.size()) go();
+        }
+        if(ans.size()) go();
+    }
+
+    sort(v.begin(), v.end(), cmp);
+
+    for(string i : v) cout << i << '\n';
     return 0;
 }
