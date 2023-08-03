0a1891acf7f0812238d1a4f735b47451aef2af61
Kim Taeyeong <tyeong1102@gmail.com>
2023-08-03 19:06:28 +0900

    꽃길

--- BOJ/Sliver/Silver2/S2_꽃길.cpp
+++ BOJ/Sliver/Silver2/S2_꽃길.cpp
@@ -1,53 +1,83 @@
 /*
-	fill()로 배열의 초기화를 해주어야 한다. 그렇지 않으면 틀렸습니다 됨.
+    처음에는 flower 함수에 체크해주는 코드까지 같이 넣어서 범위를 벗어나거나 겹칠 경우 return 0을 해주는 방식을 사용하였다.
+    하지만 그럴 경우 3개의 씨앗이 다 피지 못해도 0으로 출력되어 최솟값에 문제가 발생하여 check 함수를 따로 구현하였다.
 */
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, 1, 0, -1};
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
 
-int t, m, n, k, y, x;
-int a[51][51], visited[51][51];
+int ret = 987654321;
+int n, cnt;
+int a[14][14], visited[14][14];
 
-void dfs(int y, int x) {
-	visited[y][x] = 1;
-	for(int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-		if(a[ny][nx] == 1 && !visited[ny][nx]) {
-			dfs(ny, nx);
-		}
-	}
-	return;
+int flower(int y, int x) {
+    visited[y][x] = 1;
+
+    int price = a[y][x];
+
+    for(int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        visited[ny][nx] = 1;
+        price += a[ny][nx];
+    }
+
+    return price;
+}
+
+bool check(int y, int x) {
+    if(visited[y][x]) return 0;
+
+    for(int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if(ny < 0 || ny >= n || nx < 0 || nx >= n || visited[ny][nx]) return 0;
+    }
+
+    return 1;
+}
+
+void erase(int y, int x) {
+    visited[y][x] = 0;
+
+    for(int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        visited[ny][nx] = 0;
+    }
+}
+
+void calc(int cnt, int total) {
+    if(cnt == 3) {
+        ret = min(ret, total);
+        return;
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(check(i, j)) {
+                calc(cnt + 1, total + flower(i, j));
+                erase(i, j);
+            }
+        }
+    }
 }
 
 int main() {
-	cin >> t;
+    cin >> n;
 
-	for(int i = 0; i < t; i++) {
-		// 초기화 과정이 필요함
-		fill(&a[0][0], &a[0][0] + 51 * 51, 0);
-        fill(&visited[0][0], &visited[0][0] + 51 * 51, 0);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
 
-		int cnt = 0;
-		cin >> m >> n >> k;
+    calc(0, 0);
 
-		for(int j = 0; j < k; j++) {
-			cin >> x >> y;
-			a[y][x] = 1;
-		}
-
-		for(int j = 0; j < n; j++) {
-			for(int k = 0; k < m; k++) {
-				if(a[j][k] == 1 && !visited[j][k]){ // 해당 칸이 1이고 방문하지 않았다면
-					dfs(j, k); // dfs
-					cnt++; // 뻗어나가는게 끝나면 +1
-				}
-			} // 다시 다음 시작점을 찾음 -> 반복
-		}
-		cout << cnt << '\n';
-	}
+    cout << ret;
 }
