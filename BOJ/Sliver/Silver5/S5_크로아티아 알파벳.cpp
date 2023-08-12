126cd36a621fec0a5c87d4530d5a561fb48664d5
Kim Taeyeong <tyeong1102@gmail.com>
2023-08-12 23:50:37 +0900

    크로아티아 알파벳

--- BOJ/Sliver/Silver5/S5_크로아티아 알파벳.cpp
+++ BOJ/Sliver/Silver5/S5_크로아티아 알파벳.cpp
@@ -1,22 +1,20 @@
-/*
-    우선 문제를 봤을 때 무식하게 풀기를 생각해야 한다.
-    만약 그렇게 생각을 했는데 범위가 너무 커서 시간 복잡도가 커질 것 같으면 dp, 이분탐색, 그리디 등이 생각나야 한다.
-*/
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n; 
+string a[8] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+string s;
+int idx;
 
 int main() {
-    cin >> n; 
+    cin >> s;
 
-    int i = 666; 
-
-    for(;; i++){ // 무한루프
-        if(to_string(i).find("666") != string::npos) n--; 
-        if(n == 0) break;
+    for(int i = 0; i < 8; i++) {
+        while(true) {
+            idx = s.find(a[i]);
+            
+            if(idx == string::npos) break;
+            s.replace(idx, a[i].length(), " ");
+        }
     }
-
-    cout << i << "\n"; 
+    cout << s.length() << '\n';
 }
