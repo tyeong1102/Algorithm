874dc81674113e773aebf575565dc1784e6df556
Kim Taeyeong <tyeong1102@gmail.com>
2023-09-08 17:54:51 +0900

    소수의 연속합

--- BOJ/Gold/Gold3/G3_소수의 연속합.cpp
+++ BOJ/Gold/Gold3/G3_소수의 연속합.cpp
@@ -1,31 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, size, ret;
-string s;
-stack<int> st;
+bool che[4000001]; // 소수인지 판별하는 boolean문
+int n, a[2000001], sum, ret, p, hi, lo; // a배열에 소수로 판정된 수 저장
 
 int main() {
     cin >> n;
-    cin >> s;
 
-    st.push(-1);
+    for(int i = 2; i <= n; i++) {
+        if(che[i]) continue;
 
-    for(int i = 0; i < s.size(); i++) {
-
-        if(s[i] == '(') {
-            st.push(i);
+        for(int j = 2 * i; j <= n; j += i) {
+            che[j] = 1;
         }
+    }
 
-        if(s[i] == ')') {
-            st.pop();
+    for(int i = 2; i <= n; i++) {
+        if(!che[i]) a[p++] = i; // 소수인 수를 a배열에 저장
+    }
 
-            if(!st.empty()) {
-                ret = max(ret, i - st.top());
-            } else {
-                st.push(i);
-            }
-        }
+    while(1) {
+        if(sum >= n) sum -= a[lo++]; // sum이 n보다 큰 것이기 때문에 lo를 한칸 이동시켜 뺌
+        else if(hi == p) break;
+        else sum += a[hi++]; // sum이 더 작은 것이기 때문에 hi를 이동시켜 더함
+
+        if(sum == n) ret++; // n과 같은 sum이 나오면 하나 증가
     }
 
     cout << ret << '\n';
