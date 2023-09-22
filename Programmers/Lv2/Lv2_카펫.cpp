6ef0b28fc8ede0c9dd55772877af24c9bcab0d58
Kim Taeyeong <tyeong1102@gmail.com>
2023-09-22 18:53:37 +0900

    카펫

---
+++ Programmers/Lv2/Lv2_카펫.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    
+    for(int i = 1; i < n; i++) {
+        int sum = 0;
+        int s = i;
+        
+        while(sum < n) sum += s++;
+        if(sum == n) answer++;
+    }
+    
+    return answer;
+}
