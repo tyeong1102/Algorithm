5dee488b48916932f82ed9d3546b6dce8fc9f8e0
Kim Taeyeong <tyeong1102@gmail.com>
2023-09-27 21:10:52 +0900

    영어 끝말잇기

--- Programmers/Lv2/Lv2_영어 끝말잇기.cpp
+++ Programmers/Lv2/Lv2_영어 끝말잇기.cpp
@@ -1,17 +1,33 @@
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int solution(int n) {
-    int answer = 1;
+vector<int> solution(int n, vector<string> words) {
+    vector<int> answer;
     
-    for(int i = 1; i < n; i++) {
-        int sum = 0;
-        int s = i;
+    int idx = 0;
+    map<string, int> m;
+    
+    for(int i = 0; i < words.size() - 1; i++) {
+        m[words[i]]++;
         
-        while(sum < n) sum += s++;
-        if(sum == n) answer++;
+        if(m[words[i + 1]] > 0) { 
+            idx = i + 1;
+            break;
+        }
+        
+        if(words[i][words[i].size() - 1] != words[i + 1][0]) { // 두번째 조건
+            idx = i + 1;
+            break;
+        }
+    }
+    
+    if(idx == 0) {
+        answer.push_back(0);
+        answer.push_back(0);
+    } else {
+        answer.push_back(idx % n + 1);
+        answer.push_back(idx / n + 1);
     }
     
     return answer;
