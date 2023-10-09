6ef5e201f142c8737bbf539e87ecb72b9bdf3438
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-10-09 21:40:20 +0900

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int[] arr;
+    public static int N, v;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        v = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == v){
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
