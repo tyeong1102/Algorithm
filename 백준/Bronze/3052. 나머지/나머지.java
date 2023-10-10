069d37ea8577d5c7211194c7ee0fb91f6a88feb5
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-10-11 02:14:28 +0900

    [Bronze II] Title: 나머지, Time: 128 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] arr = new boolean[42];
+
+        for(int i = 0 ; i < 10 ; i++) {
+            arr[Integer.parseInt(br.readLine()) % 42] = true;
+        }
+
+        int cnt = 0;
+        for(boolean value : arr) {
+            if(value){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
