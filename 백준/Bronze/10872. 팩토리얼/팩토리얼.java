09aaf8b46a8d798821ea3ff3917cfe4be8edd0d2
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-10-21 02:56:53 +0900

    [Bronze V] Title: 팩토리얼, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int sum = 1;
+        
+		while(N != 0) {
+			sum = sum * N;
+			N--;
+		}
+        
+		System.out.println(sum);
+		
+	}
+}
