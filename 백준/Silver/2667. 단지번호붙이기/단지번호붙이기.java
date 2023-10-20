4978d11fba56f995783bac1b8ae16d2a446e4851
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-10-21 02:49:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 140 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static int[][] arr;
+    static int[] aparts;
+    static boolean[][] visited;
+    static int n;
+    static int area = 0;
+
+    static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {1, 0, -1, 0};
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+        aparts[area]++;
+
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+            if(arr[nx][ny] == 1 && !visited[nx][ny]) {
+                dfs(nx, ny);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1][n + 1];
+        visited = new boolean[n + 1][n + 1];
+        aparts = new int[n * n];
+
+        for(int i = 0; i < n; i++) {
+            str = br.readLine();
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(arr[i][j] == 1 && !visited[i][j]) {
+                    area++;
+                    dfs(i, j);
+                }
+            }
+        }
+
+        Arrays.sort(aparts);
+
+        System.out.println(area);
+        for(int i = 0; i < aparts.length; i++) {
+            if(aparts[i] != 0) System.out.println(aparts[i]);
+        }
+    }
+}
