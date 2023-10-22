081e0b7bb7deabea32b820206c1439d504142e8d
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-10-23 03:16:38 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 116 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < n/4; i++) {
+            sb.append("long ");
+        }
+        sb.append("int");
+        System.out.println(sb);
+    }
+}
