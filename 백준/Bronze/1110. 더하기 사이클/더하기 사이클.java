e79c28c8a5bd0879ef99a0422e9d45103da6ad5b
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-10-23 19:38:51 +0900

    [Bronze I] Title: 더하기 사이클, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        int sum = n;
+
+        while(true) {
+            n = ((n % 10) * 10) + (((n / 10) + (n % 10)) % 10);
+            cnt++;
+
+            if(sum == n) break;
+        }
+
+        System.out.println(cnt);
+    }
+}
