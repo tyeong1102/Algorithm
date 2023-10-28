ca2587eda4eddd1bf0a5cc180dae6cf7a78ba7b4
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-10-28 21:28:19 +0900

    [Silver I] Title: 영역 구하기, Time: 132 ms, Memory: 14996 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int n, m, k, cnt;
+
+    static void dfs(int x, int y) {
+        visited[x][y] = true;
+        cnt++;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (!visited[nx][ny] && arr[nx][ny] == 0) {
+                dfs(nx, ny);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        arr = new int[n + 1][m + 1];
+        visited = new boolean[n + 1][m + 1];
+
+        while (k-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            for (int i = x1; i < x2; i++) {
+                for (int j = y1; j < y2; j++) {
+                    arr[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 0 && !visited[i][j]) {
+                    cnt = 0;
+                    dfs(i, j);
+                    list.add(cnt);
+                }
+            }
+        }
+
+        Collections.sort(list);
+
+        System.out.println(list.size());
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i)).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
