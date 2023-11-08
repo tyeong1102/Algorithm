27cb12728d9332097b93661f9bb575dd8d620946
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-08 23:58:58 +0900

    [Bronze III] Title: 공 넣기, Time: 148 ms, Memory: 15900 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        int M = Integer.parseInt(st.nextToken());
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int F = Integer.parseInt(st.nextToken()); // F번 바구니부터
+            int E = Integer.parseInt(st.nextToken()); // E번 바구니까지
+            int ball = Integer.parseInt(st.nextToken()); // ball번 공을 넣는다
+
+            for(int j = F-1; j<E; j++)
+            {
+                arr[j] = ball;
+            }
+        }
+        br.close();
+        for (int baguni : arr)
+            System.out.print(baguni + " ");
+    }
+}
