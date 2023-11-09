8b261aaab9b510ce21e1df32abe0d8f42acaa13d
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-09 14:58:38 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] arr;
+    static boolean[] visited;
+    static int n, m;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void go(int at, int depth) {
+        if (m == depth) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = at; i <= n; i++) {
+            arr[depth] = i;
+            go(i + 1, depth + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        go(1,0);
+
+        System.out.println(sb);
+
+    }
+}
