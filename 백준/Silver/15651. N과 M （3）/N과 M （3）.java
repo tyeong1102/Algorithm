74926c5f27e960261d5073435dd3fa7742710328
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-09 15:40:33 +0900

    [Silver III] Title: N과 M (3), Time: 456 ms, Memory: 69132 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n, m;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void go(int depth) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            arr[depth] = i;
+            go(depth + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        go(0);
+
+        System.out.println(sb);
+
+    }
+}
