f097cce1e216a4c62bfcd8b037be8dcbecc6482a
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-10 23:40:09 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 2256 ms, Memory: 127276 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static int n;
+    public static Integer[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+
+        arr = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (int val : arr) {
+            sb.append(val).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
