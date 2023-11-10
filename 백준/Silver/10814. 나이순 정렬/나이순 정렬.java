619ed0a2b6e16e41ec94e427bdb58d36fd8d74fe
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-11 00:09:18 +0900

    [Silver V] Title: 나이순 정렬, Time: 664 ms, Memory: 47800 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n;
+    public static String[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+
+        arr = new String[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+        });
+
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
