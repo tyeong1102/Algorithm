9f622ca54f10da2d64c10722ec4bf0df505ef926
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-10 23:48:52 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 20356 ms, Memory: 110048 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static int n;
+    public static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int val : arr) {
+            sb.append(val).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
