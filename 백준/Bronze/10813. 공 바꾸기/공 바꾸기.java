d4dc627854f3fc8938d25c53636c3bbc9522e783
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-13 02:20:11 +0900

    [Bronze II] Title: 공 바꾸기, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n, m;
+    public static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            int tmp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = tmp;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            sb.append(arr[i]).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+}
