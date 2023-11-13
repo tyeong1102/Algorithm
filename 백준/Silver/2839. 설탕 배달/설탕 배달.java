bdf17cdc87133e9021903b77379a60ff2df22923
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-13 14:46:35 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        while (true) {
+            if (n % 5 == 0) {
+                System.out.println(n / 5 + cnt);
+                break;
+            } else if (n < 0) {
+                System.out.println(-1);
+                break;
+            }
+            n -= 3;
+            cnt++;
+        }
+    }
+}
