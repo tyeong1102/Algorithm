e2f8c693b1f36bf960d0794d7c2445aa1751ae6b
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-16 03:52:54 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 111 ms, Memory: 19,900 KB -BaekjoonHub

---
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/README.md
@@ -0,0 +1,15 @@
+# [D2] 어디에 단어가 들어갈 수 있을까 - 1979 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PuPq6AaQDFAUq) 
+
+### 성능 요약
+
+메모리: 19,900 KB, 시간: 111 ms, 코드길이: 2,176 Bytes
+
+### 제출 일자
+
+2023-11-16 03:52
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Solution {
+    public static int t, n, k;
+    public static int[][] arr;
+
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        t = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= t; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+            arr = new int[n + 1][n + 1];
+
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int ret = 0;
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (arr[i][j] == 1) {
+                        cnt++;
+                    }else if (cnt > 0 && arr[i][j] == 0) {
+                        if (cnt == k) {
+                            ret++;
+                        }
+                        cnt = 0;
+                    }
+                }
+                if (cnt == k) {
+                    ret++;
+                }
+                cnt = 0;
+
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (arr[j][i] == 1) {
+                        cnt++;
+                    } else if (cnt > 0 && arr[j][i] == 0) {
+                        if (cnt == k) {
+                            ret++;
+                        }
+                        cnt = 0;
+                    }
+                }
+                if (cnt == k) {
+                    ret++;
+                }
+                cnt = 0;
+            }
+
+            sb.append('#').append(test_case).append(' ').append(ret).append('\n');
+        }
+        System.out.println(sb);
+
+    }
+}
