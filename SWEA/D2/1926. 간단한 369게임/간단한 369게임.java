2792aa8758e078d23014e7c51de2a77354d31eaf
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-16 03:06:16 +0900

    [D2] Title: 간단한 369게임, Time: 99 ms, Memory: 16,872 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public static int n;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            String str = String.valueOf(i);
+
+            if (str.contains("3") || str.contains("6") || str.contains("9")) {
+                for (int j = 0; j < str.length(); j++) {
+                    if (str.charAt(j) == '3' || str.charAt(j) == '6' || str.charAt(j) == '9') {
+                        sb.append('-');
+                    }
+                }
+                sb.append(' ');
+            } else {
+                sb.append(i).append(' ');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
