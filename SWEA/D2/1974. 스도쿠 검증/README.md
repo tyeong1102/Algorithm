da14618796629d90b074ec54e914e2e9d0ad1ec0
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-16 15:04:08 +0900

    [D2] Title: 스도쿠 검증, Time: 114 ms, Memory: 19,932 KB -BaekjoonHub

---
+++ SWEA/D2/1974. 스도쿠 검증/README.md
@@ -0,0 +1,15 @@
+# [D2] 스도쿠 검증 - 1974 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5Psz16AYEDFAUq) 
+
+### 성능 요약
+
+메모리: 19,932 KB, 시간: 114 ms, 코드길이: 2,578 Bytes
+
+### 제출 일자
+
+2023-11-16 15:03
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class Solution {
+    public static int t;
+    public static int[][] arr;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        t = Integer.parseInt(br.readLine());
+
+        for(int test_case = 1; test_case <= t; test_case++) {
+
+            arr = new int[10][10];
+
+            // 행렬 저장
+            for (int i = 0; i < 9; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < 9; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            boolean flag = true; // 초기 상태는 true
+            for (int i = 0; i < 9; i++) {
+                int[] list = new int[10];
+                for (int j = 0; j < 9; j++) {
+                    list[arr[i][j]]++;
+                }
+
+                for (int k = 1; k <= 9; k++) {
+                    if (list[k] == 0) {
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 9; i++) {
+                int[] list = new int[10];
+                for (int j = 0; j < 9; j++) {
+                    list[arr[j][i]]++;
+                }
+
+                for (int k = 1; k <= 9; k++) {
+                    if (list[k] == 0) {
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i <= 6; i += 3) {
+                for (int j = 0; j <= 6; j += 3) {
+                    int[] list = new int[10];
+                    for (int x = i; x < i + 3; x++) {
+                        for (int y = j; y < j + 3; y++) {
+                            list[arr[x][y]]++;
+                        }
+                    }
+                    for (int k = 1; k <= 9; k++) {
+                        if (list[k] == 0) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            int ret = 0;
+            if (flag) {
+                ret = 1;
+            } else {
+                ret = 0;
+            }
+
+            sb.append('#').append(test_case).append(' ').append(ret).append('\n');
+
+        }
+
+        System.out.println(sb);
+
+    }
+}
