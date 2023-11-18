8a9ddd803fae3e18b8e560733b52068d478e77f4
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-18 17:07:19 +0900

    [D2] Title: 두 개의 숫자열, Time: 110 ms, Memory: 19,160 KB -BaekjoonHub

---
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.function.IntBinaryOperator;
+
+class Solution {
+    public static int t, n, m;
+    public static int[] arr1;
+    public static int[] arr2;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        t = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= t; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+
+            arr1 = new int[n];
+            arr2 = new int[m];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < arr1.length; i++) {
+                arr1[i] = Integer.parseInt(st.nextToken());
+            }
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < arr2.length; i++) {
+                arr2[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int ret = Integer.MIN_VALUE;
+
+            if (n < m) {
+                for (int i = 0; i <= m - n; i++) {
+                    int sum = 0;
+                    for (int j = 0; j < n; j++) {
+                        sum += arr1[j] * arr2[i + j];
+                    }
+
+                    ret = Math.max(ret, sum);
+                }
+            } else if (n > m) {
+                for (int i = 0; i <= n - m; i++) {
+                    int sum = 0;
+                    for (int j = 0; j < m; j++) {
+                        sum += arr1[i + j] * arr2[j];
+                    }
+
+                    ret = Math.max(ret, sum);
+                }
+            } else {
+                int sum = 0;
+                for (int i = 0; i < n; i++) {
+                    sum += arr1[i] * arr2[i];
+                }
+                ret = Math.max(ret, sum);
+            }
+
+            System.out.println("#" + test_case + " " + ret);
+        }
+    }
+}
