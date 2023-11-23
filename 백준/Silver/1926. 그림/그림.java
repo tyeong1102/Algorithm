c7e98aaa22a89310c56372c7fc41dbf9dc7954b1
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-11-23 11:37:05 +0900

    [Silver I] Title: 그림, Time: 376 ms, Memory: 52796 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,76 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static int n, m, area, cnt, ret;
+    public static int[][] arr;
+    public static boolean[][] visited;
 
-    static int n,m;
-    static int[][] arr;
-    static boolean[][] visited;
-    static int[] dx = {0, -1, 0, 1};
-    static int[] dy = {1, 0, -1, 0};
+    public static int[] dx = {1, 0, -1, 0};
+    public static int[] dy = {0, 1, 0, -1};
 
-    public static void main(String[] args) throws IOException{
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+        cnt++;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (!visited[nx][ny] && arr[nx][ny] == 1) {
+                dfs(nx, ny);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         arr = new int[n][m];
         visited = new boolean[n][m];
 
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<m; j++) {
+            for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int cnt=0;
-        int max=0;
-
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(arr[i][j] ==1 && !visited[i][j]) {
-                    max = Math.max(max, bfs(j,i));
-                    cnt++;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 1 && !visited[i][j]) {
+                    area++;
+                    dfs(i, j);
+                    ret = Math.max(ret, cnt);
+                    cnt = 0;
                 }
             }
         }
 
-        System.out.println(cnt);
-        System.out.println(max);
-    }
-
-    static int bfs(int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {x,y});
-        visited[y][x] = true;
-        
-        int size = 1;
-        
-        while(!q.isEmpty()) {
-            int[] p = q.poll();
-            int px = p[0];
-            int py = p[1];
-
-            for(int d=0; d<4; d++) {
-                int nx = px+dx[d];
-                int ny = py+dy[d];
-
-                if(nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
-                if(visited[ny][nx] || arr[ny][nx] == 0) continue;
-                if(arr[ny][nx] == 1) {
-                    size++;
-                    visited[ny][nx] = true;
-                    q.add(new int[] {nx,ny});
-                }
-            }
-        }
-        return size;
+        System.out.println(area);
+        System.out.println(ret);
     }
 }
