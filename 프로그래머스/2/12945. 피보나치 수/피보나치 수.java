d79a068d8c444520a35c9189e9904434ce0739dc
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2023-12-19 13:29:34 +0900

    [level 2] Title: 피보나치 수, Time: 8.93 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int fibo(int n) {
+        int[] arr = new int[n+1];
+        
+        arr[0] = 0;
+        arr[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+        }
+        
+        return arr[n];
+    }
+    
+    public int solution(int n) {
+        int answer = fibo(n);
+        return answer;
+    }
+}
