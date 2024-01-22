487b26280a9b946eccf9c559f1bbcdb53bbc920e
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-01-22 18:41:14 +0900

    [Gold IV] Title: 알파벳, Time: 1024 ms, Memory: 18872 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,50 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
- 
+import java.util.Scanner;
+
 public class Main {
-    public static int R, C;
-    public static String[][] graph;
-    public static boolean[][] visited;
-    public static int[] dx = {-1, 1, 0, 0};
-    public static int[] dy = {0, 0, 1, -1};
-    public static int result = 0;
- 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        graph = new String[R][C];
-        visited = new boolean[R][C];
-        for(int i=0;i<R;++i){
-            graph[i] = br.readLine().split("");
-        }
- 
-        DFS(0,0, graph[0][0], 1);
-        System.out.println(result);
-    }
- 
-    public static void DFS(int x, int y, String str, int count){
- 
-        result = Math.max(result, count);
- 
-        for (int i = 0; i < 4; ++i) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx < 0 || nx >= R || ny < 0 || ny >= C||visited[nx][ny]) {
-                continue;
-            }
-            if (!str.contains(String.valueOf(graph[nx][ny]))) {
-                visited[nx][ny] = true;
-                DFS(nx, ny, str+graph[nx][ny],count + 1);
-                visited[nx][ny] = false;
+    static int R, C, ans;
+    static char[][] map;
+    static boolean[] visit = new boolean[26];
+    static int[] dr = { -1, 1, 0, 0 };
+    static int[] dc = { 0, 0, -1, 1 };
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        R = sc.nextInt();
+        C = sc.nextInt();
+        map = new char[R][C];
+        for (int i = 0; i < R; i++) {
+            String str = sc.next();
+            for (int j = 0; j < C; j++) {
+                map[i][j] = str.charAt(j);
             }
         }
- 
+        ans = 0;
+        visit[map[0][0] - 'A'] = true;
+        DFS(0, 0, 1);
+        System.out.println(ans);
     }
- 
+
+    private static void DFS(int i, int j, int cnt) {
+        if (ans < cnt)
+            ans = cnt;
+        else if (ans == 26)
+            return;
+        for (int dir = 0; dir < dr.length; dir++) {
+            int nr = i + dr[dir];
+            int nc = j + dc[dir];
+
+            if (isRange(nr, nc) && !visit[map[nr][nc] - 'A']) {
+                visit[map[nr][nc] - 'A'] = true;
+                DFS(nr, nc, cnt + 1);
+                visit[map[nr][nc] - 'A'] = false;
+            }
+        }
+
+    }
+
+    private static boolean isRange(int nr, int nc) {
+        if (0 <= nr && nr < R && 0 <= nc && nc < C)
+            return true;
+        return false;
+    }
 }
