8b9dc5505934f9227d2a3ab7b92243a0751503c1
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-01-30 11:55:56 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 160 ms, Memory: 23032 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n, k;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            q.add(i);
+        }
+
+        sb.append("<");
+
+        while (q.size() > 1) {
+            for (int i = 0; i < k - 1; i++) {
+                q.add(q.poll());
+            }
+            sb.append(q.poll()).append(", ");
+        }
+
+        sb.append(q.poll()).append(">");
+        System.out.println(sb);
+    }
+}
