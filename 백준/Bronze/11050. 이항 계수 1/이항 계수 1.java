dbc7facf2a15ff4f6a7319015e0c4befab18f124
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-01-30 12:07:14 +0900

    [Bronze I] Title: 이항 계수 1, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n, k;
+
+    public static int factorial(int n) {
+        if (n <= 1)	{
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        System.out.println(factorial(n) / (factorial(n - k) * factorial(k)));
+    }
+}
