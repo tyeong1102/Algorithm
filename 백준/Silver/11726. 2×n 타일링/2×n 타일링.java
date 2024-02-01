8cd13dd9a82f9d4f9da5c2c50a8a5d7cd50ba5a3
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-02-01 23:22:20 +0900

    [Silver III] Title: 2×n 타일링, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -14,9 +14,8 @@ public class Main {
 
         dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
 
-        for (int i = 3; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
 
