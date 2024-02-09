09812ee159b880b559e84ff6602ca39faa9aa1d5
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-02-09 23:43:12 +0900

    [Silver II] Title: 최소 힙, Time: 1108 ms, Memory: 33972 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int tmp = Integer.parseInt(br.readLine());
+
+            if (tmp == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            } else {
+                pq.add(tmp);
+            }
+        }
+    }
+}
