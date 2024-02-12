55a7ae77e6324367a20986106bbe9663c549137c
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-02-12 22:22:04 +0900

    [Silver IV] Title: 대칭 차집합, Time: 760 ms, Memory: 85708 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int a, b, ret;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        HashSet<Integer> setA = new HashSet<>();
+        HashSet<Integer> setB = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < a; i++) {
+            setA.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < b; i++) {
+            setB.add(Integer.parseInt(st.nextToken()));
+        }
+
+        for (int n : setA) {
+            if (!setB.contains(n)) {
+                ret++;
+            }
+        }
+
+        for (int n : setB) {
+            if (!setA.contains(n)) {
+                ret++;
+            }
+        }
+
+        System.out.println(ret);
+    }
+}
