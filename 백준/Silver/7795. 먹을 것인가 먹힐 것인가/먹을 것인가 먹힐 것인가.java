36e1e19ca008e634fe148a9d45ee8f73f35b0368
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-03-16 15:02:22 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 480 ms, Memory: 39696 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -1,54 +1,57 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-class Main {
+public class Main {
+    public static int t, x, y, ret;
+    public static int[] a, b;
 
-    public static void main(String[] args) throws Exception {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
+        t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
 
-            int[] A = new int[N];
-            int[] B = new int[M];
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            a = new int[x];
+            b = new int[y];
 
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                A[j] = Integer.parseInt(st.nextToken());
+            for(int i = 0; i < x; i++) {
+                a[i] = Integer.parseInt(st.nextToken());
             }
 
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                B[j] = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < y; i++) {
+                b[i] = Integer.parseInt(st.nextToken());
             }
 
-            Arrays.sort(A);
-            Arrays.sort(B);
+            Arrays.sort(b);
 
-            int result = 0;
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < M; k++) {
-                    if (A[j] <= B[k]) {
-                        break;
-                    }
-                    else {
-                        result += 1;
+            ret = 0;
+            for (int i = 0; i < x; i++) {
+                int s = 0;
+                int e = y - 1;
+                int idx = 0;
+
+                while (s <= e) {
+                    int mid = (s + e) / 2;
+                    if (b[mid] < a[i]) {
+                        s = mid + 1;
+                        idx = mid + 1;
+                    } else {
+                        e = mid - 1;
                     }
                 }
+                ret += idx;
             }
 
-            bw.write(Integer.toString(result) + "\n");
+            System.out.println(ret);
         }
-
-       bw.flush();
-       bw.close();
-       br.close();
     }
 }
