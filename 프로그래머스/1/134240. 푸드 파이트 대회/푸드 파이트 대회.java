f69770c9d9fc6a613303a8d2cd1e39f76ba59870
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-03-19 19:29:27 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 15.10 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -3,30 +3,14 @@ import java.io.*;
 
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for(int i = 1; i < food.length; i++) {
-            int n = food[i] / 2;
-            map.put(i, n);
-        }
-        
-        
-        for(int i = 1; i <= map.size(); i++) {
-            for(int j = 0; j < map.get(i); j++) {
-                answer += Integer.toString(i);
+       String answer = "0";
+
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
             }
         }
-        
-        answer += "0";
-        
-        for(int i = map.size(); i >= 1; i--) {
-            for(int j = 0; j < map.get(i); j++) {
-                answer += Integer.toString(i);
-            }
-        }
-        
+
         return answer;
     }
 }
