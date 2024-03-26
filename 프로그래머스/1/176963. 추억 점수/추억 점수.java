b71243f8d05ff30d8943204f57e1bb8e12e57af9
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-03-26 15:45:20 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,29 @@
+// 추억점수 = 그리움 점수 합한 값
+
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        
+        for(int i = 0; i < photo.length; i++) {
+            for(int j = 0; j < photo[i].length; j++) {
+                if(!map.containsKey(photo[i][j])) {
+                    answer[i] += 0;
+                } else {
+                    answer[i] += map.get(photo[i][j]);
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+}
