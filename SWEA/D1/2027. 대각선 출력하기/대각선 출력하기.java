637a6c86d7997402f782e8ebf821c01bbb396972
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-11 18:50:48 +0900

    [D1] Title: 대각선 출력하기, Time: 96 ms, Memory: 23,852 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution {
+    public static int t;
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        t = 1;
+
+        for (int test_case = 1; test_case <= t; test_case++) {
+
+            System.out.println("#++++");
+            System.out.println("+#+++");
+            System.out.println("++#++");
+            System.out.println("+++#+");
+            System.out.println("++++#");
+        }
+    }
+}
