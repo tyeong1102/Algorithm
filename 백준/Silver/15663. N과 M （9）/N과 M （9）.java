dd0cf4b5b99fa2d77ae3f4187084cedeb98c524b
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-16 02:34:13 +0900

    [Silver II] Title: N과 M (9), Time: 208 ms, Memory: 20928 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n, m;
+    public static int[] arr, nums;
+    public static boolean[] visited;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        visited = new boolean[n + 1];
+        nums = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(nums);
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int cnt) {
+        if (cnt == m) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = 0;
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) continue;
+
+            if (before != nums[i]) {
+                visited[i] = true;
+                arr[cnt] = nums[i];
+                before = nums[i];
+                dfs(cnt + 1);
+                visited[i] = false;
+            }
+
+        }
+    }
+}
