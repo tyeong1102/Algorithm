f60ccc3d4ae08af9c4a9cae9c3320a696936e390
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-21 18:49:47 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 644 ms, Memory: 50752 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n, m, x, y;
+    public static int[][] arr, ret;
+    public static boolean[][] visited;
+
+    public static int[] dx = {1, 0, -1, 0};
+    public static int[] dy = {0, 1, 0, -1};
+
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int cx = now[0];
+            int cy = now[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (arr[nx][ny] == 1 && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    ret[nx][ny] = ret[cx][cy] + 1;
+                    q.add(new int[]{nx, ny});
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+        ret = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] == 2) {
+                    x = i;
+                    y = j;
+                }
+                if (arr[i][j] == 0) {
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        bfs(x, y);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j]) {
+                    ret[i][j] = -1;
+                }
+                sb.append(ret[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
