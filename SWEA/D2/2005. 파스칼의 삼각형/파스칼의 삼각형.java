266137cf2d872a1af813769e8aad53dea3b869a9
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-27 17:11:15 +0900

    [D2] Title: 파스칼의 삼각형, Time: 108 ms, Memory: 20,960 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Solution {
     public static int t, n;
@@ -8,30 +9,34 @@ class Solution {
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         t = Integer.parseInt(br.readLine());
 
-        for(int test_case = 1; test_case <= t; test_case++) {
+        for (int test_case = 1; test_case <= t; test_case++) {
             n = Integer.parseInt(br.readLine());
+
             arr = new int[n][n];
 
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j <= i; j++) {
-                    if (i == j || j == 0) {
-                        arr[i][j] = 1;
-                    } else {
-                        arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
-                    }
+                arr[i][0] = 1;
+            }
+
+            for (int i = 1; i < n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
                 }
             }
 
-            System.out.println("#" + test_case);
+            sb.append("#").append(test_case).append("\n");
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j <= i; j++) {
-                    System.out.print(arr[i][j] + " ");
+                    sb.append(arr[i][j]).append(" ");
                 }
-                System.out.println();
+                sb.append("\n");
             }
         }
+
+        System.out.println(sb);
     }
 }
