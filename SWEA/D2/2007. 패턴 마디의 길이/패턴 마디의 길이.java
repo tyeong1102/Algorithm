cd4d883593fd3a6248ee2476028d6916d306d8da
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-28 16:34:28 +0900

    [D2] Title: 패턴 마디의 길이, Time: 93 ms, Memory: 20,516 KB -BaekjoonHub

--- SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
@@ -3,26 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Solution {
-    public static int t;
+    public static int t, ret;
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         t = Integer.parseInt(br.readLine());
 
-        for(int test_case = 1; test_case <= t; test_case++) {
+        for (int test_case = 1; test_case <= t; test_case++) {
             String str = br.readLine();
-            for (int i = 1; i <= str.length(); i++) {
-                String a = str.substring(0, i);
-                String b = str.substring(i, i + i);
 
-                if (a.equals(b)) {
-                    System.out.println("#" + test_case + " " + a.length());
+            int num = str.length();
+
+            for (int i = 1; i <= num; i++) {
+                String s1 = str.substring(0, i);
+                String s2 = str.substring(i, i + s1.length());
+
+                if (s1.equals(s2)) {
+                    ret = s1.length();
                     break;
                 }
             }
 
-
+            sb.append("#").append(test_case).append(" ").append(ret).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
