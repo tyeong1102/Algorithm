fe3867f578c1ae6e2c336f6f595a473ba0d7735f
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-29 16:38:13 +0900

    [D2] Title: 지그재그 숫자, Time: 101 ms, Memory: 19,800 KB -BaekjoonHub

--- SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
@@ -1,28 +1,33 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Solution {
     public static int t, n;
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         t = Integer.parseInt(br.readLine());
 
-        for(int test_case = 1; test_case <= t; test_case++) {
-
+        for (int test_case = 1; test_case <= t; test_case++) {
             n = Integer.parseInt(br.readLine());
 
+            sb.append("#").append(test_case).append(" ");
+
             int ret = 0;
-            for(int i = 1; i <= n; i++) {
-                if(i % 2 == 1) {
+
+            for (int i = 1; i <= n; i++) {
+                if (i % 2 == 1) {
                     ret += i;
                 } else {
                     ret -= i;
                 }
             }
-
-            System.out.println("#" + test_case + " " + ret);
+            sb.append(ret).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
