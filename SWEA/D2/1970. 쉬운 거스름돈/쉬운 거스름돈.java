57669da0bf8f37e965e3b263455dbd6ff9bf7f22
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-29 16:33:37 +0900

    [D2] Title: 쉬운 거스름돈, Time: 96 ms, Memory: 19,688 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -1,31 +1,30 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Solution {
     public static int t, n;
-    public static int[] cost = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
-    public static int[] arr;
+    public static int[] money = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         t = Integer.parseInt(br.readLine());
 
-        for(int test_case = 1; test_case <= t; test_case++) {
-
+        for (int test_case = 1; test_case <= t; test_case++) {
             n = Integer.parseInt(br.readLine());
-            arr = new int[cost.length];
 
-            for(int i = 0; i < cost.length; i++) {
-                arr[i] = n / cost[i];
-                n %= cost[i];
+            sb.append("#").append(test_case).append("\n");
+
+            for (int i = 0; i < money.length; i++) {
+                sb.append(n / money[i]).append(" ");
+                n %= money[i];
             }
 
-            System.out.println("#" + test_case);
-            for(int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
+            sb.append("\n");
         }
+
+        System.out.println(sb);
     }
 }
