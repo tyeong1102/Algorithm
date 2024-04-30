8d1265d0e44c377e7d8127e976944342a2011259
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-04-30 19:32:47 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 124 ms, Memory: 29,200 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
@@ -7,38 +6,39 @@ class Solution {
     public static int n, ret;
     public static int[] arr;
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         for(int test_case = 1; test_case <= 10; test_case++) {
-            StringBuilder sb = new StringBuilder();
             n = Integer.parseInt(br.readLine());
             arr = new int[n];
 
+            ret = 0;
+
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int i = 0; i < n; i++){
+            for (int i = 0; i < n; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            ret = 0;
-
             for (int i = 2; i < n - 2; i++) {
-                int cnt = Integer.MAX_VALUE;
+                int num = arr[i];
+                int min = Integer.MAX_VALUE;
                 for (int j = i - 2; j < i; j++) {
-                    cnt = Math.min(arr[i] - arr[j], cnt);
-                }
-                for (int j = i + 1; j <= i + 2; j++) {
-                    cnt = Math.min(arr[i] - arr[j], cnt);
+                    min = Math.min(min, num - arr[j]);
                 }
 
-                if (cnt > 0) {
-                    ret += cnt;
+                for (int j = i + 1; j <= i + 2; j++) {
+                    min = Math.min(min, num - arr[j]);
+                }
+
+                if (min > 0) {
+                    ret += min;
                 }
             }
-
-            sb.append('#').append(test_case).append(' ').append(ret);
-
-            System.out.println(sb);
+            sb.append("#").append(test_case).append(" ").append(ret).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
