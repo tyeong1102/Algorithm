ef05837012376d72ba54e4058f0f924a290a8af8
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-05-03 00:00:36 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 151 ms, Memory: 38,868 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
@@ -1,42 +1,42 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		
-		Scanner sc = new Scanner(System.in);
-        int T = 10;
+class Solution {
+    public static int n, ret;
+    public static int[][] arr;
 
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-            int cnt = 0;
-            int n = sc.nextInt();
-            int[][] arr = new int[100][100];
+        for(int test_case = 1; test_case <= 10; test_case++) {
+            n = Integer.parseInt(br.readLine());
+            arr = new int[n][n];
+            ret = 0;
 
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    arr[i][j] = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            for(int i = 0; i < n; i++) {
-                int tmp = 0;
-                for(int j = 0; j < n; j++) {
-                    if(arr[j][i] == 1) {
-                        tmp = 1;
-                    } else if(arr[j][i] == 2) {
-                        if(tmp == 1){
-                            cnt++;
-                        }
-                        tmp = 2;
+            for (int i = 0; i < n; i++) {
+                boolean check = false;
+                for (int j = 0; j < n; j++) {
+                    if (arr[j][i] == 1) {
+                        check = true;
+                    }
+                    if (arr[j][i] == 2 && check) {
+                        ret++;
+                        check = false;
                     }
                 }
             }
-
-            System.out.println("#" + test_case + " " + cnt);
+            sb.append("#").append(test_case).append(" ").append(ret).append("\n");
         }
-	}
+
+        System.out.println(sb);
+    }
 }
