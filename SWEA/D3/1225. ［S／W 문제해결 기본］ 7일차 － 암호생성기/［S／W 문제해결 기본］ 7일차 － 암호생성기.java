69b7b0bae144859826bf0d0a4610b032b1335c26
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-05-06 17:51:10 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 124 ms, Memory: 33,700 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -5,39 +5,51 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Solution {
-    public static int cnt;
+    public static int t, num;
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        for (int test_case = 1; test_case <= 10; test_case++) {
-            int test = Integer.parseInt(br.readLine());
+        for(int test_case = 1; test_case <= 10; test_case++) {
+            t = Integer.parseInt(br.readLine());
             Queue<Integer> q = new LinkedList<>();
 
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 0; i < 8; i++) {
-                q.add(Integer.parseInt(st.nextToken()));
+                int num = Integer.parseInt(st.nextToken());
+                q.add(num);
             }
 
-            int cnt = 1;
-            while (true) {
-                int tmp = q.poll() - cnt;
+            boolean flag = true;
 
-                if(tmp <= 0) {
-                    q.add(0);
-                    break;
-                } else {
-                    q.add(tmp);
+            while (true) {
+                for (int i = 1; i <= 5; i++) {
+                    num = q.poll() - i;
+
+                    if (num <= 0) {
+                        q.add(0);
+                        flag = false;
+                        break;
+                    } else {
+                        q.add(num);
+                    }
                 }
 
-                cnt = (cnt % 5) + 1;
+                if (!flag) {
+                    break;
+                }
             }
 
-            System.out.print("#" + test_case + " ");
-            for(int i = 0; i < 8; i++) {
-                System.out.print(q.poll() + " ");
+            sb.append("#").append(test_case).append(" ");
+
+            for (int num : q) {
+                sb.append(num).append(" ");
             }
-            System.out.println();
+
+            sb.append("\n");
         }
+
+        System.out.println(sb);
     }
 }
