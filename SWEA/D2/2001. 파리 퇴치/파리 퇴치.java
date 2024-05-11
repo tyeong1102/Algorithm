f04742c43ca513493162e6fc08acdaaf9349b5f0
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-05-11 17:11:43 +0900

    [D2] Title: 파리 퇴치, Time: 106 ms, Memory: 23,868 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,25 +1,24 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.io.FileInputStream;
 
 class Solution {
-    public static int n, m, t;
+    public static int t, n, m, ret;
     public static int[][] arr;
 
     public static void main(String args[]) throws Exception {
-//        System.setIn(new FileInputStream("src/input (22).txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         t = Integer.parseInt(br.readLine());
 
-        for(int test_case = 1; test_case <= t; test_case++) {
-            StringBuilder sb = new StringBuilder();
+        for (int test_case = 1; test_case <= t; test_case++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
             arr = new int[n][n];
+            ret = 0;
 
             for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -28,22 +27,23 @@ class Solution {
                 }
             }
 
-            int ret = Integer.MIN_VALUE;
             for (int i = 0; i <= n - m; i++) {
                 for (int j = 0; j <= n - m; j++) {
                     int sum = 0;
-                    for (int x = 0; x < m; x++) {
-                        for (int y = 0; y < m; y++) {
-                            sum += arr[i + x][j + y];
+                    for (int k = i; k < i + m; k++) {
+                        for (int l = j; l < j + m; l++) {
+                            sum += arr[k][l];
                         }
                     }
+
                     ret = Math.max(ret, sum);
                 }
             }
 
-            sb.append('#').append(test_case).append(' ').append(ret);
+            sb.append("#").append(test_case).append(" ").append(ret).append("\n");
 
-            System.out.println(sb);
         }
+
+        System.out.println(sb);
     }
 }
