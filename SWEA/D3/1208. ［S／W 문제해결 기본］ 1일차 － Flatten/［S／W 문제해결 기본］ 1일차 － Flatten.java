9cf8b4610adff201d1b35420d583bfb6f7555747
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-05-11 16:17:30 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 133 ms, Memory: 30,004 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -11,36 +11,35 @@ class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        for(int test_case = 1; test_case <= 10; test_case++) {
+        for (int test_case = 1; test_case <= 10; test_case++) {
             n = Integer.parseInt(br.readLine());
             arr = new int[100];
+            ret = 0;
 
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 0; i < 100; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            for (int i = 0; i < n; i++) {
+            while (n-- > 0) {
                 Arrays.sort(arr);
 
-                if (arr[99] - arr[0] == 1) {
+                int num = arr[99] - arr[0];
+
+                if (num <= 1) {
                     ret = 1;
-                    return;
+                    break;
                 }
 
-                if (arr[0] == arr[99]) {
-                    ret = 0;
-                    return;
-                }
-
-                arr[0]++;
                 arr[99]--;
+                arr[0]++;
             }
 
             Arrays.sort(arr);
             ret = arr[99] - arr[0];
 
             sb.append("#").append(test_case).append(" ").append(ret).append("\n");
+
         }
 
         System.out.println(sb);
