b8433c85f11905b1880216e53b0a067d376bf9ec
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-07-10 09:56:13 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -6,55 +6,55 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static int n, m;
+    public static int[][] arr;
+    public static boolean[][] visited;
 
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
-    static int n, m;
-    static boolean[][] visited;
-    static int[][] arr;
+    public static int[] dx = {1, 0, -1, 0};
+    public static int[] dy = {0, 1, 0, -1};
+
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int cx = now[0];
+            int cy = now[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (!visited[nx][ny] && arr[nx][ny] == 1) {
+                    q.add(new int[]{nx, ny});
+                    arr[nx][ny] = arr[cx][cy] + 1;
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         arr = new int[n][m];
-        for(int i=0; i<n; i++) {
-            String s = br.readLine();
-            for(int j=0; j<m; j++) {
-                arr[i][j] = s.charAt(j) - '0';
-            }
-        }
-
         visited = new boolean[n][m];
-        visited[0][0] = true;
-        bfs(0, 0);
-        System.out.println(arr[n-1][m-1]);
-    }
 
-    public static void bfs(int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {x,y});
-
-        while(!q.isEmpty()) {
-            int now[] = q.poll();
-            int nowX = now[0];
-            int nowY = now[1];
-
-            for(int i=0; i<4; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-
-                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m)
-                    continue;
-                if (visited[nextX][nextY] || arr[nextX][nextY] == 0)
-                    continue;
-
-                q.add(new int[] {nextX, nextY});
-                arr[nextX][nextY] = arr[nowX][nowY] + 1;
-                visited[nextX][nextY] = true;
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = str.charAt(j) - '0';
             }
         }
+
+        bfs(0, 0);
+
+        System.out.println(arr[n - 1][m - 1]);
     }
 }
