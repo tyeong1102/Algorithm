69af0ea9ba30164e95a9f9ac17207768c267c6ad
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-11-06 13:59:23 +0900

    [Silver III] Title: 수열, Time: 1584 ms, Memory: 23856 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int n, k, s, e, max;
+    public static int n, k, s, e, ret;
     public static int[] arr;
 
     public static void main(String[] args) throws IOException {
@@ -14,26 +14,29 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
         arr = new int[n];
+        ret = Integer.MIN_VALUE;
 
         st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         s = 0;
         e = s + k - 1;
-        max = Integer.MIN_VALUE;
 
         for (int i = 0; i <= n - k; i++) {
-            int tmp = 0;
+            int sum = 0;
             for (int j = s; j <= e; j++) {
-                tmp += arr[j];
+                sum += arr[j];
             }
-            max = Math.max(max, tmp);
             s++;
             e++;
+
+            ret = Math.max(ret, sum);
         }
 
-        System.out.println(max);
+        System.out.println(ret);
+
     }
 }
