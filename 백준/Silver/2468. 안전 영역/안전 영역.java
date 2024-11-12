4490fd1a46f8f02f610db8b4b762caee25b01cb0
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-11-12 10:33:40 +0900

    [Silver I] Title: 안전 영역, Time: 208 ms, Memory: 18740 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,69 +1,63 @@
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.util.ArrayList;
-    import java.util.Collections;
-    import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    public class Main {
-        static int[][] arr;
-        static boolean[][] visited;
-        static int n, cnt;
+public class Main {
+    public static int n, tmp, height, cnt, ret;
+    public static int[][] arr;
+    public static boolean[][] visited;
 
-        static int[] dx = {1, 0, -1, 0};
-        static int[] dy = {0, 1, 0, -1};
+    public static int[] dx = {1, 0, -1, 0};
+    public static int[] dy = {0, 1, 0, -1};
 
-        static void dfs(int x, int y, int height) {
-            visited[x][y] = true;
+    public static void dfs(int x, int y, int h) {
+        visited[x][y] = true;
 
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-                if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                if (arr[nx][ny] > height && !visited[nx][ny]) {
-                    dfs(nx, ny, height);
-                }
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+            if(!visited[nx][ny] && arr[nx][ny] > h) {
+                dfs(nx, ny, h);
             }
-
-        }
-
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
-
-            n = Integer.parseInt(br.readLine());
-
-            arr = new int[n + 1][n + 1];
-
-            int tmp = 0;
-            for (int i = 0; i < n; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < n; j++) {
-                    arr[i][j] = Integer.parseInt(st.nextToken());
-                    tmp = Math.max(tmp, arr[i][j]);
-                }
-            }
-
-            ArrayList<Integer> list = new ArrayList<>();
-            for (int t = 0; t <= tmp; t++) {
-                visited = new boolean[n + 1][n + 1];
-                cnt = 0;
-
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        if (arr[i][j] > t && !visited[i][j]) {
-                            dfs(i, j, t);
-                            cnt++;
-                        }
-                    }
-                }
-                list.add(cnt);
-            }
-
-            Collections.sort(list);
-
-            System.out.println(list.get(list.size() - 1));
-
         }
     }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1][n + 1];
+        ret = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                tmp = Math.max(tmp, arr[i][j]);
+            }
+        }
+        
+        while (height < tmp) {
+            visited = new boolean[n + 1][n + 1];
+            cnt = 0;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (!visited[i][j] && arr[i][j] > height) {
+                        dfs(i, j, height);
+                        cnt++;
+                    }
+                }
+            }
+
+            ret = Math.max(ret, cnt);
+            height++;
+        }
+        
+        System.out.println(ret);
+    }
+}
