c5cbe0a900ab1090bf3b927973bd0cdd17c26da6
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-04 14:58:25 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,45 +1,47 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Stack;
- 
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < T; i++) {
-			sb.append(solve(br.readLine())).append('\n');
-		}
-		
-		System.out.println(sb);
-	}
- 
-	public static String solve(String s) {
- 
-		Stack<Character> stack = new Stack<>();
- 
-		for (int i = 0; i < s.length(); i++) {
- 
-			char c = s.charAt(i);
- 
-			if (c == '(') {
-				stack.push(c);
-			} else if (stack.empty()) {
-				return "NO";
-			} else {
-				stack.pop();
-			}
-		}
-        
-		if (stack.empty()) {
-			return "YES";
-		} else {
-			return "NO";
-		}
-	}
+    public static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        while (n-- > 0) {
+            Queue<Character> q = new LinkedList<>();
+            String str = br.readLine();
+
+            if (str.charAt(0) == ')') {
+                sb.append("NO").append("\n");
+            } else {
+                for (int i = 0; i < str.length(); i++) {
+                    char c = str.charAt(i);
+                    if (c == '(') {
+                        q.add(c);
+                    } else {
+                        if(!q.isEmpty() && q.peek() == '(') {
+                            q.poll();
+                        } else {
+                            q.add(c);
+                        }
+                    }
+                }
+
+                if (!q.isEmpty()) {
+                    sb.append("NO").append("\n");
+                } else {
+                    sb.append("YES").append("\n");
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
