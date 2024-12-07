119fa21e5dc17d398e53f1b5b5e3836c2fc4a0e0
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-07 23:56:04 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13980 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,9 @@
+import java.io.IOException;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hello World!");
+    }
 }
