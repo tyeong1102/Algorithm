5e5b9f3a1164fd1229c03dab70ba2da449eef073
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-08 15:52:21 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13884 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,10 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("         ,r'\"7");
-        System.out.println("r`-_   ,'  ,/");
-        System.out.println(" \\. \". L_r'");
-        System.out.println("   `~\\/");
-        System.out.println("      |");
-        System.out.println("      |");
-    }
+	public static void main(String[] args) {
+		String s = "         ,r'\"7\n" +
+				   "r`-_   ,'  ,/\n" + 
+				   " \\. \". L_r'\n" + 	
+				   "   `~\\/\n" + 		
+				   "      |\n" + 		
+				   "      |";
+ 
+		System.out.print(s);
+	}
 }
