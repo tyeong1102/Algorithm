f22b36304f4d75b8d47b6da9d906933f81da3ea7
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-08 17:19:52 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 132 ms, Memory: 16360 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static int odd;
+    public static String ret;
+    public static int[] arr = new int[26];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            arr[str.charAt(i) - 'A']++;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 1) {
+                odd++;
+            }
+        }
+
+        if (odd > 1) {
+            System.out.println("I'm Sorry Hansoo");
+            return;
+        } else {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i] / 2; j++) {
+                    sb.append((char) (i + 'A'));
+                }
+            }
+            ret = sb.toString();
+
+            String tmp = sb.reverse().toString();
+
+            sb = new StringBuilder();
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] % 2 == 1) {
+                    sb.append((char) (i + 'A'));
+                }
+            }
+
+            ret += sb + tmp;
+            System.out.println(ret);
+        }
+    }
+}
