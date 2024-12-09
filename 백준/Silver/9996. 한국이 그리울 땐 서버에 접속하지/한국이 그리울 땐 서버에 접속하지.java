de8a9c1081be5577d2b9a1006b7c9cb0a91e3aba
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-09 15:59:14 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -4,29 +4,24 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static int n;
-    public static String str;
+    public static String pattern;
+    public static String[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        str = br.readLine();
 
-        String[] arr = str.split("\\*");
+        n = Integer.parseInt(br.readLine());
+        pattern = br.readLine();
+        arr = pattern.split("\\*");
 
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
-            String s = br.readLine();
+            String str = br.readLine();
 
-            if(arr[0].length() + arr[1].length() > s.length()) {
-                sb.append("NE").append("\n");
-                continue;
-            }
-
-            String front = s.substring(0, arr[0].length());
-            String back = s.substring(s.length() - arr[1].length(), s.length());
-
-            if (front.equals(arr[0]) && back.equals(arr[1])) {
+            if (str.length() >= arr[0].length() + arr[1].length() &&
+                    str.startsWith(arr[0]) &&
+                    str.endsWith(arr[1])) {
                 sb.append("DA").append("\n");
             } else {
                 sb.append("NE").append("\n");
@@ -34,6 +29,5 @@ public class Main {
         }
 
         System.out.println(sb);
-
     }
 }
