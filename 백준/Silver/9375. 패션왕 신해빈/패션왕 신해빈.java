437df91428dd66f36c4e26751b422761546b41a8
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-09 16:21:52 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -2,40 +2,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static int t, n;
+    public static int t, n, ret;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         t = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
+
         while (t-- > 0) {
             n = Integer.parseInt(br.readLine());
+            ret = 1;
+
             HashMap<String, Integer> map = new HashMap<>();
 
             for (int i = 0; i < n; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                st.nextToken();
+                String str = br.readLine();
+                String[] arr = str.split(" ");
 
-                String cloth = st.nextToken();
-
-                if (map.containsKey(cloth)) {
-                    map.put(cloth, map.get(cloth) + 1);
-                } else {
-                    map.put(cloth, 1);
-                }
+                map.put(arr[1], map.getOrDefault(arr[1], 0) + 1);
             }
 
-            int ret = 1;
-
-            for (int num : map.values()) {
-                ret *= (num + 1);
+            for (String str : map.keySet()) {
+                ret *= (map.get(str) + 1);
             }
 
-            System.out.println(ret - 1);
+            sb.append(ret - 1).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
