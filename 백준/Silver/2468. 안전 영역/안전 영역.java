00418f9f9497fec131da0aefd92a636485af0c62
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-11 15:17:39 +0900

    [Silver I] Title: 안전 영역, Time: 256 ms, Memory: 20160 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -4,14 +4,14 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int n, tmp, height, cnt, ret;
+    public static int n, water, top, area, ret;
     public static int[][] arr;
     public static boolean[][] visited;
 
     public static int[] dx = {1, 0, -1, 0};
     public static int[] dy = {0, 1, 0, -1};
 
-    public static void dfs(int x, int y, int h) {
+    public static void dfs(int x, int y) {
         visited[x][y] = true;
 
         for (int i = 0; i < 4; i++) {
@@ -19,8 +19,9 @@ public class Main {
             int ny = y + dy[i];
 
             if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-            if(!visited[nx][ny] && arr[nx][ny] > h) {
-                dfs(nx, ny, h);
+            if (!visited[nx][ny] && arr[nx][ny] != 0) {
+                dfs(nx, ny);
+                visited[nx][ny] = true;
             }
         }
     }
@@ -30,34 +31,44 @@ public class Main {
         StringTokenizer st;
 
         n = Integer.parseInt(br.readLine());
-        arr = new int[n + 1][n + 1];
-        ret = Integer.MIN_VALUE;
+        arr = new int[n][n];
+
+        top = Integer.MIN_VALUE;
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
-                tmp = Math.max(tmp, arr[i][j]);
+                top = Math.max(top, arr[i][j]);
             }
         }
-        
-        while (height < tmp) {
-            visited = new boolean[n + 1][n + 1];
-            cnt = 0;
+
+        while (water < top) {
+            visited = new boolean[n][n];
+            area = 0;
 
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (!visited[i][j] && arr[i][j] > height) {
-                        dfs(i, j, height);
-                        cnt++;
+                    if (arr[i][j] == water) {
+                        arr[i][j] = 0;
                     }
                 }
             }
 
-            ret = Math.max(ret, cnt);
-            height++;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (arr[i][j] != 0 && !visited[i][j]) {
+                        dfs(i, j);
+                        area++;
+                    }
+                }
+            }
+
+            ret = Math.max(ret, area);
+            water++;
         }
-        
+
         System.out.println(ret);
+
     }
 }
