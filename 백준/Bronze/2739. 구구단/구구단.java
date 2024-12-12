e74d4fd4c8dbe4e11b6b0f54dc1b21cadaee6878
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-12 21:03:04 +0900

    [Bronze V] Title: 구구단, Time: 100 ms, Memory: 14008 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -3,15 +3,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static int N;
+    public static int n;
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
-        for(int i = 1; i <= 9; i++) {
-            System.out.println(N + " * " + i + " = " + N * i);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 9; i++) {
+            sb.append(n).append(" * ").append(i).append(" = ").append(n * i).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
