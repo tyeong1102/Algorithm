7da4db42ad0d3e0f5e4208aa6813d5abcc92f5f5
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-13 23:16:04 +0900

    [Bronze V] Title: 문자와 문자열, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -3,13 +3,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static int i;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = br.readLine();
-        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        i = Integer.parseInt(br.readLine());
 
-        System.out.println(s.charAt(n - 1));
+        System.out.println(str.charAt(i - 1));
     }
 }
