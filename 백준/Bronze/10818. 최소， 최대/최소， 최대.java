3914d4f0c4a7e2303c656874bb51bb6887ec94a5
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-13 23:06:34 +0900

    [Bronze III] Title: 최소, 최대, Time: 1220 ms, Memory: 105128 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -5,21 +5,25 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static int n;
+    public static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
 
-        st = new StringTokenizer(br.readLine());
-
-        for(int i = 0; i < N; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        System.out.println(arr[0] + " " + arr[N - 1]);
+        StringBuilder sb = new StringBuilder();
+        sb.append(arr[0]).append(" ").append(arr[n - 1]);
+
+        System.out.println(sb);
     }
 }
