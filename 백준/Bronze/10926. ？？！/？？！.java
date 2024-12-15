0051739cd27143898bb5794d545ae32ce67d6548
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-15 12:41:17 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,15 +1,18 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String lastString = "??!";
-		
-		String s = br.readLine();
-		System.out.println(s + lastString);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str= br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(str).append("??!");
+
+        System.out.println(sb);
+    }
 }
