e77a140f580116bee8f46412d63caa92d54fa7db
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-16 22:35:07 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 96 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -4,54 +4,37 @@ import java.io.InputStreamReader;
 import java.util.List;
 
 public class Main {
-    public static int mo, ja;
-    public static boolean flag;
-    public static String str;
     public static List<Character> list = List.of('a', 'e', 'i', 'o', 'u');
+    public static int mo, mCnt, jCnt;
+    public static boolean flag;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         while (true) {
-            str = br.readLine();
-
+            String str = br.readLine();
             mo = 0;
+            mCnt = 0;
+            jCnt = 0;
+
+            flag = true;
 
             if (str.equals("end")) {
                 break;
             }
 
-            flag = true;
-            for (int i = 0; i < str.length(); i++) {
+            for(int i = 0; i < str.length(); i++) {
                 if(list.contains(str.charAt(i))) {
                     mo++;
-                }
-            }
-
-            for(int i = 1; i < str.length(); i++) {
-                if (str.charAt(i) == str.charAt(i - 1)) {
-                    if (str.charAt(i) == 'e' || str.charAt(i) == 'o') {
-                        continue;
-                    } else {
-                        flag = false;
-                        break;
-                    }
-                }
-            }
-
-            for (int i = 0; i <= str.length() - 3; i++) {
-                int jNum = 0;
-                int mNum = 0;
-                for (int j = i; j < i + 3; j++) {
-                    if (!list.contains(str.charAt(j))) {
-                        jNum++;
-                    } else {
-                        mNum++;
-                    }
+                    mCnt++;
+                    jCnt = 0;
+                } else {
+                    jCnt++;
+                    mCnt = 0;
                 }
 
-                if (jNum == 3 || mNum == 3) {
+                if(mCnt == 3 || jCnt == 3) {
                     flag = false;
                 }
             }
@@ -60,11 +43,22 @@ public class Main {
                 flag = false;
             }
 
-            if (flag) {
-                sb.append("<").append(str).append(">").append(" is acceptable.").append("\n");
-            } else {
-                sb.append("<").append(str).append(">").append(" is not acceptable.").append("\n");
+            for(int i = 1; i < str.length(); i++) {
+                if (str.charAt(i - 1) == str.charAt(i)) {
+                    if (str.charAt(i) == 'e' || str.charAt(i) == 'o') {
+                        continue;
+                    } else {
+                        flag = false;
+                    }
+                }
             }
+
+            if (flag) {
+                sb.append("<").append(str).append("> is acceptable.").append("\n");
+            } else {
+                sb.append("<").append(str).append("> is not acceptable.").append("\n");
+            }
+
         }
 
         System.out.println(sb);
