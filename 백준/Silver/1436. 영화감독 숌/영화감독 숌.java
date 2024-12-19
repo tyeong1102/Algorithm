d648e2794399b47eb27d2f96f7629bd655498631
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-19 17:32:48 +0900

    [Silver V] Title: 영화감독 숌, Time: 268 ms, Memory: 85736 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,24 +1,22 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-
+ 
 public class Main {
-    public static int n;
-    
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-
-        int num = 666;
-        int cnt = 1;
-
-        while(cnt != n) {
-            num++;
-            if(String.valueOf(num).contains("666")) {
-                cnt++;
-            }
-        }
-        System.out.println(num);
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+ 
+		int num = 666;
+		int count = 1;
+        
+		while(count != N) {
+			num++;
+			if(String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+		System.out.println(num);
+	}
 }
