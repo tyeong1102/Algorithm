a2d2758a281966b59e35438de8b4a791e3e1f540
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-22 12:41:05 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -6,10 +6,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String s = br.readLine(); 
-        char c = s.charAt(0);  
-        
-        System.out.print((int)c);  
+
+        String str = br.readLine();
+
+        System.out.println(str.charAt(0) - 0);
     }
 }
