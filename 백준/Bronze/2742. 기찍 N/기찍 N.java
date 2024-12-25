496d65397738c7ea57df416e6d11a6589a2ecd4d
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-25 14:40:22 +0900

    [Bronze IV] Title: 기찍 N, Time: 184 ms, Memory: 18128 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -1,17 +1,20 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-        
-		while( N > 0 ) {
-			System.out.println(N);
-			N--;
-		}	
-	}
+    public static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = n; i > 0; i--) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
- 
