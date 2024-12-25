ef9c26b5b246547bd61055fee05db38f92e8fc5c
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-26 01:13:37 +0900

    [Silver IV] Title: 수 찾기, Time: 672 ms, Memory: 43372 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,21 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static int n, m;
     public static int[] arr;
 
+    public static int binarySearch(int key) {
+
+        int lo = 0;
+        int hi = arr.length - 1;
+
+        while (lo <= hi) {
+
+            int mid = (lo + hi) / 2;
+
+            if (key < arr[mid]) {
+                hi = mid - 1;
+            } else if (key > arr[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
@@ -25,15 +48,15 @@ public class Main {
         m = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < m; i++) {
             int num = Integer.parseInt(st.nextToken());
 
-            int in = Arrays.binarySearch(arr, num);
-
-            if (in < 0) {
-                sb.append(0 + "\n");
+            if (binarySearch(num) >= 0) {
+                sb.append(1).append("\n");
             } else {
-                sb.append(1 + "\n");
+                sb.append(0).append("\n");
             }
         }
 
