e8040a355e869e0763d368668d65d7494f307577
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-25 14:23:16 +0900

    [Bronze V] Title: A+B - 8, Time: 128 ms, Memory: 16068 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -1,23 +1,28 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.io.IOException;
- 
+
 public class Main {
-	public static void main(String args[]) throws IOException {
- 
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		int A;
-		int B;
-        
-		StringTokenizer st;
-		for (int i = 1; i <= T; i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
-			System.out.println("Case #" + i + ": " + A + " + " + B + " = " + (A+B));
-		}
-	}
+    public static int t, a, b;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= t; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            sb.append("Case #").append(i).append(": ").append(a + " + " + b + " = ").append(a + b).append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
