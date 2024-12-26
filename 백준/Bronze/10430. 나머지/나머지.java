671f1d40f607e6018b9cfe916352ba62547ae8b7
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-27 03:01:10 +0900

    [Bronze V] Title: 나머지, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -2,22 +2,23 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
+    public static int a, b, c;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        System.out.println((a + b) % c);
+        System.out.println((a % c + b % c) % c);
+        System.out.println((a * b) % c);
+        System.out.println((a % c * b % c) % c);
         
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
- 		String str = br.readLine();
-		StringTokenizer st = new StringTokenizer(str," ");
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-        
-		System.out.println( (A+B)%C );
-		System.out.println( (A%C + B%C)%C );
-		System.out.println( (A*B)%C );
-		System.out.println( (A%C * B%C)%C );
     }
 }
