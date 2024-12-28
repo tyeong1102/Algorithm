34a418ccb5a937d87cc2fd029a968b755357f400
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-28 10:49:37 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,23 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        boolean[] arr = new boolean[42];
 
-        for(int i = 0 ; i < 10 ; i++) {
-            arr[Integer.parseInt(br.readLine()) % 42] = true;
-        }
+        List<Integer> list = new ArrayList<>();
 
-        int cnt = 0;
-        for(boolean value : arr) {
-            if(value){
-                cnt++;
+        for (int i = 0; i < 10; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            int tmp = num % 42;
+
+            if (!list.contains(tmp)) {
+                list.add(tmp);
             }
         }
-        System.out.println(cnt);
+
+        System.out.println(list.size());
     }
 }
