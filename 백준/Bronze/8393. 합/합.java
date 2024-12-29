f78e47246e98d3e3a15ebd9522b29fb0ed794610
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-29 23:57:07 +0900

    [Bronze V] Title: 합, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,17 +1,19 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-        
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine());
-		int s = 0;
- 
-		for( int i = 1 ; i <= N ; i++ ) s+=i;
- 
-		System.out.println(s);
-	}
+    public static int n, sum;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
 }
