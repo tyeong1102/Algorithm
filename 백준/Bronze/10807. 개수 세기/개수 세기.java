8d2235c33e4df44199a0715df2c2d118adcbb3f4
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2024-12-31 14:34:50 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,34 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[] arr;
-    public static int N, v;
+    public static int n, v;
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        arr = new int[N];
-
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         v = Integer.parseInt(br.readLine());
 
-        int cnt = 0;
-
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == v){
-                cnt++;
-            }
+        if (map.containsKey(v)) {
+            System.out.println(map.get(v));
+        } else {
+            System.out.println(0);
         }
 
-        System.out.println(cnt);
     }
 }
