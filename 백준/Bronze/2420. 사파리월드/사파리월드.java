475189046357d6c0f31fb83ce4fd1ed19c646bf8
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-01-01 23:48:26 +0900

    [Bronze V] Title: 사파리월드, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2420. 사파리월드/사파리월드.java
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -1,13 +1,20 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static long n, m, ret;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        long n = Long.parseLong(st.nextToken());  
-        long m = Long.parseLong(st.nextToken());
-        System.out.print(Math.abs(n-m)); 
+
+        n = Long.parseLong(st.nextToken());
+        m = Long.parseLong(st.nextToken());
+
+        ret = Math.abs(n - m);
+
+        System.out.println(ret);
     }
 }
