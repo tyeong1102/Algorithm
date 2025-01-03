7f36b4ef57e7bb0c767e02d5796af9268ab81c08
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-01-03 14:41:33 +0900

    [Bronze II] Title: 숫자의 개수, Time: 104 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -3,20 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static long a, b, c, mul;
+    public static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr = new int[10];
-        int num = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+        a = Integer.parseInt(br.readLine());
+        b = Integer.parseInt(br.readLine());
+        c = Integer.parseInt(br.readLine());
+        arr = new int[10];
 
-        String str = String.valueOf(num);
+        mul = a * b * c;
 
-        for (int i = 0; i < str.length(); i++) {
-            arr[(str.charAt(i) - 48)]++;
+        while (mul > 0) {
+            int tmp = (int) (mul % 10);
+            arr[tmp]++;
+            mul /= 10;
         }
 
-        for (int v : arr) {
-            System.out.println(v);
+        StringBuilder sb = new StringBuilder();
+        for (int num : arr) {
+            sb.append(num).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
