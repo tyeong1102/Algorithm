53c4834fd6e27b4f477f50b85371ec509b1f027c
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-01-06 03:48:53 +0900

    [Bronze V] Title: 꼬마 정민, Time: 108 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -4,14 +4,17 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static long a, b, c, ret;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        long A = Long.parseLong(st.nextToken());
-        long B = Long.parseLong(st.nextToken());
-        long C = Long.parseLong(st.nextToken());
+        a = Long.parseLong(st.nextToken());
+        b = Long.parseLong(st.nextToken());
+        c = Long.parseLong(st.nextToken());
+        ret = a + b + c;
 
-        System.out.println(A + B + C);
+        System.out.println(ret);
     }
 }
