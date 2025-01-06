ce0f9363ecd73a5918074afb332d7bb193df9098
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-01-07 02:51:41 +0900

    [Bronze II] Title: OX퀴즈, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -1,38 +1,44 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    public static int t, ret, cnt;
+    public static String str;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        t = Integer.parseInt(br.readLine());
+
         StringBuilder sb = new StringBuilder();
-        
-        int t = Integer.parseInt(br.readLine());
 
-        String arr[] = new String[t];
+        while (t-- > 0) {
+            str = br.readLine();
+            boolean flag = false;
+            ret = 0;
+            cnt = 0;
 
-        for (int i = 0; i < t; i++) {
-            arr[i] = br.readLine();
-        }
-        
-        for (int i = 0; i < t; i++) {
-
-            int cnt = 0;
-            int sum = 0;
-
-            for (int j = 0; j < arr[i].length(); j++) {
-
-                if (arr[i].charAt(j) == 'O') {
-                    cnt++;
+            for (int i = 0; i < str.length(); i++) {
+                int c = str.charAt(i);
+                if (c == 'O') {
+                    if (flag) {
+                        cnt++;
+                        ret += cnt;
+                    } else {
+                        flag = true;
+                        cnt++;
+                        ret += cnt;
+                    }
                 } else {
+                    flag = false;
                     cnt = 0;
                 }
-                sum += cnt;
             }
 
-            sb.append(sum).append('\n');
+            sb.append(ret).append("\n");
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
