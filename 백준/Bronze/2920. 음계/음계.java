4272aa417b0a9f4faccef6b073e11f3ebe4f1f64
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-01-07 20:27:24 +0900

    [Bronze II] Title: 음계, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -4,26 +4,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[8];
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        arr = new int[8];
 
         for (int i = 0; i < 8; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        String ret = "";
-        for (int i = 0; i < 7; i++) {
-            if (arr[i + 1] == arr[i] + 1) {
-                ret = "ascending";
-            } else if (arr[i + 1] == arr[i] - 1) {
-                ret = "descending";
-            } else {
-                ret = "mixed";
-                break;
-            }
-        }
 
-        System.out.print(ret);
+        int tmp = arr[0];
+
+        if (tmp == 1) {
+            for (int i = 1; i < 8; i++) {
+                if (tmp + 1 == arr[i]) {
+                    tmp++;
+                } else {
+                    System.out.println("mixed");
+                    return;
+                }
+            }
+
+            System.out.println("ascending");
+        } else if (tmp == 8) {
+            for (int i = 1; i < 8; i++) {
+                if (tmp - 1 == arr[i]) {
+                    tmp--;
+                } else {
+                    System.out.println("mixed");
+                    return;
+                }
+            }
+
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
     }
 }
