e1576bc770ca8b41ceb5c3e16b21498d0eafd8f4
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-01-09 19:48:27 +0900

    [Bronze III] Title: 직각삼각형, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -2,40 +2,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-    
+    public static int x, y, z;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       
- 
-        while(true) {
-        	StringTokenizer st = new StringTokenizer(br.readLine()," ");
-            
-        	int x = Integer.parseInt(st.nextToken());
-        	int y = Integer.parseInt(st.nextToken());
-        	int z = Integer.parseInt(st.nextToken());
-        	
-        	
-			// 0 0 0 을 입력받으면 종료
-        	if(x == 0 && y == 0 && z == 0) break;
-            
-        	
-        	if((x * x + y * y) == z * z) {
-				System.out.println("right");
-			}
-        	else if(x * x == (y * y + z * z)) {
-				System.out.println("right");
-			}
-        	else if(y * y == (z * z + x * x)) {
-				System.out.println("right");
-			}
-        	else {
-				System.out.println("wrong");
-			}
-        	
-		}
- 
-	}
- 
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+
+            st = new StringTokenizer(br.readLine());
+
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            z = Integer.parseInt(st.nextToken());
+
+            if (x == 0 && y == 0 && z == 0) {
+                break;
+            }
+
+            if((x * x + y * y) == z * z) {
+                sb.append("right").append('\n');
+            }
+            else if(x * x == (y * y + z * z)) {
+                sb.append("right").append('\n');
+            }
+            else if(y * y == (z * z + x * x)) {
+                sb.append("right").append('\n');
+            }
+            else {
+                sb.append("wrong").append('\n');
+            }
+        }
+
+        System.out.println(sb);
+
+    }
 }
