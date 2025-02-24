30c3b161ee4c54534f4b831e93216f0f5340cf87
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-02-25 00:08:19 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -3,19 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static int n;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
-        for (int i = n; i > 0; i--) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = i; j > 0; j--) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i; j > 0; j--) {
                 sb.append("*");
             }
-            System.out.println(sb);
+
+            sb.append("\n");
         }
 
-
+        System.out.println(sb);
     }
 }
