91e3b6c91ded7e616011a207c9478b2ad0802c2a
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-02-25 01:42:33 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 112 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -1,42 +1,42 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
- 
-		for (int i = 0; i < N; i++) {	// 1 ~ N
- 
-			for (int j = 0; j < i; j++) {
-				sb.append(' ');
-			}
- 
-			for (int k = 0; k < (2 * N - 1) - (2 * i); k++) {
-				sb.append('*');
-			}
- 
-			sb.append('\n');
-		}
- 
-		for (int i = 0; i < N - 1; i++) {	// N+1 ~ 2N-1
-        
-			for (int j = 1; j < (N - 1) - i; j++) {
-				sb.append(' ');
-			}
- 
-			for (int k = 0; k < 3 + 2 * i; k++) {
-				sb.append('*');
-			}
-			sb.append('\n');
-		}
-        
-        
-		System.out.println(sb);
-	}
- 
+    public static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+                sb.append(" ");
+            }
+
+            for (int k = 0; k < (2 * n - 1) - (2 * i); k++) {
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 1; j < (n - 1) - i; j++) {
+                sb.append(" ");
+            }
+
+            for (int k = 0; k < 3 + 2 * i; k++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
