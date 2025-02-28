de3e7635f5f507daa016172d310f71b2468e1912
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-02-28 16:37:13 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -3,17 +3,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static int n;
-    public static int[] dp;
+    static int n;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
         dp = new int[1001];
-
-        dp[0] = 1;
-        dp[1] = 1;
+        dp[0] = dp[1] = 1;
 
         for (int i = 2; i <= n; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
