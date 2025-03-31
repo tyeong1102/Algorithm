eaf9068700b409e8c7bd7ea4fe48ca56c39525ca
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-03-31 17:09:22 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static int n;
@@ -8,15 +9,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(st.nextToken());
         dp = new int[1001];
 
+        dp[0] = 1;
         dp[1] = 1;
         dp[2] = 3;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
         }
 
         System.out.println(dp[n]);
