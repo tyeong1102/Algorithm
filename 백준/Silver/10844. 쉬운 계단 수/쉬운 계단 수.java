9c2769b746cd848c9432373c56b98f43655adc62
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-04-09 15:03:51 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -4,14 +4,13 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static int n;
-    public static long num = 1000000000;
-    public static long[][] dp;
+    public static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
-        dp = new long[n+1][10];
+        dp = new int[n + 1][11];
 
         for (int i = 1; i < 10; i++) {
             dp[1][i] = 1;
@@ -19,22 +18,24 @@ public class Main {
 
         for (int i = 2; i <= n; i++) {
             for (int j = 0; j < 10; j++) {
-                if (j == 0) {
-                    dp[i][0] = dp[i - 1][1] % num;
-                } else if (j == 9) {
-                    dp[i][9] = dp[i - 1][8] % num;
-                } else {
-                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % num;
+
+                if (j > 0) {
+                    dp[i][j] += dp[i - 1][j - 1];
                 }
+                if (j < 9) {
+                    dp[i][j] += dp[i - 1][j + 1];
+                }
+
+                dp[i][j] %= 1000000000;
             }
         }
 
         long ret = 0;
 
         for (int i = 0; i < 10; i++) {
-            ret += dp[n][i];
+            ret = (ret + dp[n][i]) % 1000000000;
         }
 
-        System.out.println(ret % num);
+        System.out.println(ret);
     }
 }
