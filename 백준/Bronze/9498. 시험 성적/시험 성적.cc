55ce1f9c2e374a359d389e98215d9d113a11f7dc
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-11-27 12:48:56 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int score;
+  cin >> score;
+
+  if (score >= 90) {
+    cout << "A" << "\n";
+  } else if (score >= 80) {
+    cout << "B" << "\n";
+  } else if (score >= 70) {
+    cout << "C" << "\n";
+  } else if (score >= 60) {
+    cout << "D" << "\n";
+  } else {
+    cout << "F" << "\n";
+  }
+}
