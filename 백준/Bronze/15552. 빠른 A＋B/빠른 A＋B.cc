a02bd99f8897a0e269e2f3fd3e70bf926588296f
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-12-19 00:40:47 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 188 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int t, a, b;
+  cin >> t;
+
+  for (int i = 0; i < t; i++) {
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b << "\n";
+  }
+}
